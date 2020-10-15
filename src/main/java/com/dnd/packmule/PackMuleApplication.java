8236ab7b3e391aa8ea3ee12cb57c80f3fd65a6fa
Mark Creamer <bubzor888@gmail.com>
2020-10-14 21:25:10 -0500

    switching to java

---
+++ src/main/java/com/dnd/packmule/PackMuleApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.packmule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackMuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackMuleApplication.class, args);
+	}
+
+}
